319eb4669f4c9f8e4d4def3b5c25d8bd600a8b41
Robay <34374361+Rofromthetrap@users.noreply.github.com>
2026-05-24 17:15:13 +0300

    added employee model, testing python model, schema and packages yml

---
+++ Python/py_snowflake_super_store_sales/snowflake_super_store_sales/models/staging/my_dbt_py.py
@@ -0,0 +1,10 @@
+from snowflake.snowpark.functions import count, col
+
+def model(dbt, session):
+
+    returns_df = dbt.ref("returns")
+
+    final_df = returns_df.select(count(col("order_id")).alias("total_returns"))
+    #final_df = returns_df.group_by("order_id").count()
+
+    return final_df
