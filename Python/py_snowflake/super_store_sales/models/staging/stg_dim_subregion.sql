ea62caed31f0c298811e40f4af85c0044f1bcaf6
Robay <34374361+Rofromthetrap@users.noreply.github.com>
2026-05-26 19:14:37 +0300

    Refactor stg_dim_subregion.sql to correct REGION_KEY mapping and ensure proper surrogate key generation

--- Python/py_snowflake/super_store_sales/models/staging/stg_dim_subregion.sql
+++ Python/py_snowflake/super_store_sales/models/staging/stg_dim_subregion.sql
@@ -3,15 +3,16 @@ with source_data as (
 ),
 denormalised as (
 SELECT DISTINCT
+   {{ dbt_utils.generate_surrogate_key(['postal_code','city']) }} as SUBREGION_KEY,
     POSTAL_CODE POSTAL_CODE_KEY,
     COUNTRY_REGION,
     CITY,
     STATE_PROVINCE,
     CASE
-        WHEN SHIP_MODE= 'West'  THEN 1
-        WHEN SHIP_MODE = 'East'  THEN 2
-        WHEN SHIP_MODE = 'Central'  THEN 3
-        WHEN SHIP_MODE = 'South'  THEN 4
+        WHEN REGION= 'West'  THEN 1
+        WHEN REGION = 'East'  THEN 2
+        WHEN REGION = 'Central'  THEN 3
+        WHEN REGION = 'South'  THEN 4
     END REGION_KEY
 FROM
 source_data
